b5c08e7fea98fe992c7417235167b57cd069dcc6
H P O M E N <H P O M E N@LAPTOP-OCOVAM4M>
2024-08-12 19:25:19 +0300

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASS3ADVANCED
+{
+    public class Book
+    {
+        public Book(string iSBN, string title, string[] authors, DateTime publicationDate, decimal price)
+        {
+            ISBN = iSBN;
+  
+            PublicationDate = publicationDate;
+            Price = price;  
+            Authors = authors;
+        }
+        public string[] Authors { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; } 
+      
+      
+
+        public override string ToString() => ($" ISBN: {ISBN} ,Title {Title} , Author {Authors} , PublicationDate {PublicationDate} , Price {Price} ");
+    }
+}
---
+++ BookFunction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASS3ADVANCED
+{
+
+    public delegate string BookFunctionDelegate(Book B);
+    public class BookFunction
+    {
+        public static string GetTitle (Book B)
+        {
+
+        return B.ISBN.ToString ();
+        }
+        public static string GetAuthors(Book B)
+        {
+
+            return B.Authors.ToString();
+        }
+        public static string GetPrice(Book B)
+        {
+
+            return B.Price.ToString();
+        }
+    }
+
+    
+}
